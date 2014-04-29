73be578e7c1e8f7a50105aba591bac24f174a075
DAVID <DAVID@DAVID-PC>
2014-04-29 16:48:42 -0500

    mas cambios

--- login.service.subsystem/src/main/java/co/edu/uniandes/csw/login/service/AuthService.java
+++ login.service.subsystem/src/main/java/co/edu/uniandes/csw/login/service/AuthService.java
@@ -19,18 +19,36 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.TEXT_HTML)
 public class AuthService {
 
+    /**
+     * Servicio que ofrece el html con el nobre de usuario y el link de cierre
+     * de sesion
+     *
+     * @param req Contexto del usuario que llama al servicio de donde se
+     * obtienen los datos de sesion
+     */
     @GET
     @Path("session/userstack")
+
     public String getLogedUserStack(@Context HttpServletRequest req) {
-        
-        return "<b>"+req.getRemoteUser()+"</b></br><a href=\"webresources/auth/session/logout\">Log Out</a>";
+
+        return "<b>" + req.getRemoteUser() + "</b></br><a href=\"webresources/auth/session/logout\">Log Out</a>";
     }
 
+    /**
+     * Servicio que deslogea al usuario que lo invoca, y luego lo redirige a la
+     * página de logout.html
+     *
+     * @param req Contexto del usuario que llama al servicio de donde se
+     * obtienen o modifican los datos de sesion
+     * @param res Contexto del usuario en la respuesta del servicio
+     */
     @GET
     @Path("session/logout")
     public void logout(@Context HttpServletRequest req, @Context HttpServletResponse res) {
         try {
+            //Se invalida la sesión del usuario que llama al servicio
             req.getSession().invalidate();
+            //Se le indica redirigirse a la página de logout
             res.sendRedirect(req.getContextPath() + "/logout.html");
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -38,6 +56,14 @@ public class AuthService {
 
     }
 
+    /**
+     * Servicio que autentica un usuario ante el sistema
+     *
+     * @param req Contexto del usuario que llama al servicio de donde se
+     * obtienen o modifican los datos de sesion
+     * @param usr nombre de usuario a autenticar
+     * @param pwd contraseña del usuario
+     */
     @POST
     public String appLogin(@Context HttpServletRequest req, String usr, String pwd) {
         try {
@@ -48,12 +74,19 @@ public class AuthService {
         }
 
     }
-    
+
+    /**
+     * Servicio que retruna el nombre del usuario autenticado. Si no lo está
+     * retorna null
+     *
+     * @param req Contexto del usuario que llama al servicio de donde se
+     * obtienen los datos de sesion
+     */
     @GET
     @Path("session/user")
     public String getLogedUser(@Context HttpServletRequest req) {
-        
+
         return req.getRemoteUser();
     }
-    
+
 }
